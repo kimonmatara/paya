97e200eb64535fbb46cafb81c9511d25b636581b
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2022-06-18 18:18:31 +0300

    Changes to name management
    - Simplified signature of conformElems / Name / make() under paya.lib.names

--- lib/names.py
+++ lib/names.py
@@ -6,7 +6,7 @@ import re
 
 import pymel.util as _pu
 
-from paya.util import short, pad
+from paya.util import short
 import paya.config as config
 import paya.lib.suffixes as _suf
 
@@ -36,17 +36,13 @@ def legalise(name):
     else:
         return '_'
 
-@short(padding='pad')
-def conformElems(*elems, padding=None):
+def conformElems(*elems):
     """
     Cleans up user name elements, typically specified through ``*args``
     and / or the ``name/n`` keyword argument.
 
     :param elems: one or more name elements, packed or unpacked
     :type elems: int, str
-    :param padding/pad: optional padding depth for any integers; defaults
-        to None
-    :type padding/pad: None, int
     :return: The conformed elements.
     :rtype: list
     """
@@ -54,11 +50,7 @@ def conformElems(*elems, padding=None):
 
     for elem in _pu.expandArgs(*elems):
         if isinstance(elem, int):
-            if padding is None:
-                elem = str(elem)
-
-            else:
-                elem = pad(elem, padding)
+            elem = str(elem)
 
         elif not elem:
             continue
@@ -67,130 +59,94 @@ def conformElems(*elems, padding=None):
 
     return out
 
-class Name(object):
+
+class Name:
     """
     Context manager. Accumulates name elements hierarchically.
 
     :param elems: one or more name elements, packed or unpacked
     :type elems: int, str
-    :param padding/pad: optional padding depth for any integers; defaults
-        to None
-    :param bool inherit/i: inherit from containing blocks; defaults to True
-    :param suffix/suf: optional override for ``config.autoSuffix``;
-        defaults to None
-    :type suffix/suf: None, bool
     """
     __elems__ = []
-    __suffix__ = None
 
-    @short(inherit='i', suffix='suf', padding='pad')
-    def __init__(self, *elems, inherit=True, suffix=None, padding=None):
-        self._elems = conformElems(*elems, padding=padding)
-        self._inherit = inherit
-
-        if suffix is None:
-            suffix = Name.__suffix__
-
-        elif isinstance(suffix, str):
-            raise TypeError("'suffix' must be a boolean")
-
-        self._suffix = suffix
+    def __init__(self, *elems):
+        self._elems = _pu.expandArgs(*elems)
 
     def __enter__(self):
-        self._old_elems = Name.__elems__
+        self._prev_elems = Name.__elems__
+        Name.__elems__ += conformElems(self._elems)
 
-        new_elems = self._elems
-
-        if self._inherit:
-            new_elems = self._old_elems + new_elems
-
-        Name.__elems__ = new_elems
-
-        self._old_suffix = Name.__suffix__
-        Name.__suffix__ = self._suffix
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        Name.__elems__ = self._old_elems
-        Name.__suffix__ = self._old_suffix
-
+        Name.__elems__ = self._prev_elems
         return False
 
-
 @short(
-    name='n',
-    inherit='i',
+    inheritName='inn',
     suffix='suf',
-    nodeType='nt',
-    padding='pad'
+    name='n',
+    nodeType='nt'
 )
 def make(
         *elems,
         name=None,
-        inherit=True,
+        inheritName=True,
         suffix=None,
         node=None,
-        nodeType=None,
-        padding=None
+        nodeType=None
 ):
     """
     Constructs Maya node names.
-
-    :param elems: one or more name elements, packed or unpacked
-    :type elems: int, str
-    :param name/n: pass-through for any elements provided via a ``name/n``
-        keyword argument; these will always be prepended; defaults to None
-    :type name/n: None, str
-    :param padding/pad: optional padding depth for any integers; defaults
-        to None
-    :type padding/pad: None, int
-    :param bool inherit/i: inherit prefixes from :class:`Name` blocks;
+    :param \*elems: one or more name elements
+    :type \*elems: int, str, list
+    :param name/n: elements passed-through via a ``name`` argument;
+        these will always be prepended to \*elems; defaults to None
+    :type name/n: int, str, list, None
+    :param bool inheritName/inn: inherit names from :class:`Name` blocks;
         defaults to True
-    :param suffix/suf: optional override for ``config.autoSuffix``; if
-        ``True``, apply suffixes; if ``False``, omit them; if a string,
-        use it; defaults to None
-    :type suffix/suf: bool, str
-    :param node: ignored if ``nodeType`` is provided; a node to inspect
-        for an appropriate suffix lookup; defaults to None
-    :type node: PyNode, str, None
-    :param nodeType/nt: ignored if ``suffix`` is a string; if provided, will
-        be used as the suffix lookup; defaults to None
-    :type nodeType/nt: bool, None
-    :return: The name.
+    :param bool suffix/suf: if string, append; if ``True``, look up a type
+        suffix; if ``False``, omit suffix; defaults to
+        :attr:`~paya.config.autoSuffix`
+    :param node: ignored if ``nodeType`` has been provided; a node to inspect
+        to determine the node type suffix; defaults to None
+    :type node: None, str, :class:`~pymel.core.general.PyNode`
+    :param nodeType/nt: a reference node type for the suffix lookups; defaults
+        to None
+    :type nodeType/nt: None, str
+    :return: A Maya node name.
     :rtype: str
     """
-    # Resolve elements
+    elems = list(_pu.expandArgs(*elems))
 
-    elems = conformElems(name, elems, padding=padding)
+    if name:
+        elems = list(_pu.expandArgs(name)) + elems
 
-    if inherit:
-        elems = Name.__elems__ + elems
+    elems = conformElems(elems)
 
-    # Resolve suffix
+    if inheritName:
+        if Name.__elems__:
+            elems = Name.__elems__ + elems
 
-    if suffix is None:
-        if inherit:
-            suffix = Name.__suffix__
+    if isinstance(suffix, str):
+        elems.append(suffix)
 
-    if suffix is None:
-        suffix = config.autoSuffix
+    else:
+        if suffix is None:
+            suffix = config.autoSuffix
 
-    if suffix:
-        _suffix = None
-
-        if isinstance(suffix, str):
-            _suffix = suffix
-
-        else:
+        if suffix:
             if nodeType is None:
                 if node:
                     nodeType = _suf.getKeyFromNode(node)
 
-            if nodeType is not None:
-                _suffix = _suf.suffixes.get(nodeType)
+            if nodeType:
+                suffix = _suf.suffixes.get(nodeType)
 
-        if _suffix is not None:
-            elems.append(_suffix)
+            if suffix:
+                elems.append(suffix)
 
     name = '_'.join(elems)
+    name = legalise(name)
 
-    return legalise(name)
+    return name
---
+++ nodetypes/ikHandle.py
@@ -0,0 +1,18 @@
+class IkHandle:
+
+    @classmethod
+    def create(
+            cls,
+            startJoint=None,
+            endEffector=None,
+
+            createRootAxis=False,
+            curve=None,
+            forceSolver=False,
+            freezeJoints=False,
+            numSpans=None,
+            parentCurve=False,
+            
+
+
+    ):
