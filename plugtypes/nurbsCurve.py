dd2b24be8d2a78883596583f33505c55ffd3a59a
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2022-07-16 15:43:59 +0300

    More sampling routines
    + info()
    + length()
    + controlPoints()

--- plugtypes/nurbsCurve.py
+++ plugtypes/nurbsCurve.py
@@ -5,7 +5,42 @@ import paya.runtime as r
 
 class NurbsCurve:
 
-    #-----------------------------------------------|    Shape Edits
+    #---------------------------------------------------|    Sampling
+
+    @short(reuse='re')
+    def info(self, reuse=True):
+        """
+        :param bool reuse/re: Reuse any previously-connected ``curveInfo``
+            node; defaults to True
+        :return: A ``curveInfo`` node connected to this curve output.
+        :rtype: :class:`~paya.runtime.nodes.CurveInfo`
+        """
+        if reuse:
+            existing = self.outputs(type='curveInfo')
+
+            if existing:
+                return existing[0]
+
+        node = r.nodes.CurveInfo.createNode()
+        self >> node.attr('inputCurve')
+        return node
+
+    def length(self):
+        """
+        :return: The arc length of this curve output.
+        :rtype: :class:`~paya.runtime.plugs.Math1D`
+        """
+        return self.info().attr('arcLength')
+
+    def controlPoints(self):
+        """
+        :return: The ``.controlPoints`` multi-attribute of a connected
+            ``curveInfo`` node.
+        :rtype: :class:`~paya.runtime.plugs.Vector`
+        """
+        return self.info().attr('controlPoints')
+
+    #---------------------------------------------------|    Edits
 
     @short(select='sel')
     def detach(self, *parameters, select=None):
