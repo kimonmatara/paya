961acc4bd26b817f33bad8db9e1177c0fe74d98a
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2022-07-26 04:43:15 +0300

    Update nurbsutil.py
    + itemsAsBezierAnchors()

--- lib/nurbsutil.py
+++ lib/nurbsutil.py
@@ -38,3 +38,59 @@ def conformUParamArg(uParam):
         return r.Attribute(uParam)
 
     raise TypeError("Can't conform U parameter: {}".format(uParam))
+
+def itemsAsBezierAnchors(items):
+    """
+    :param items: indices, point positions, or whatever else can be mapped
+        to CVs; assumes a full CV range
+    :type items: list, tuple
+    :return: The items organised into a list of dicts, where:
+
+    [
+        {'root': item, 'out': item}, # first
+        {'in': item, 'root': item, 'out': item}, # internal
+        {'in': item, 'root': item} # last
+    ]
+
+    :rtype: [dict]
+    """
+    items = list(items)
+    numCVs = len(items)
+    numAnchors = (numCVs + 2) / 3
+
+    if numAnchors.is_integer():
+        numAnchors = int(numAnchors)
+
+    else:
+        raise ValueError("Invalid number of CVs for a bezier curve.")
+
+    out = []
+
+    for i in range(numAnchors):
+        if i is 0:
+            first, last = True, False
+
+        elif i is numAnchors-1:
+            first, last = False, True
+
+        else:
+            first = last = False
+
+        group = {}
+
+        if first:
+            group['root'] = items.pop(0)
+            group['out'] = items.pop(0)
+
+        elif last:
+            group['in'] = items.pop(0)
+            group['root'] = items.pop(0)
+
+        else:
+            group['in'] = items.pop(0)
+            group['root'] = items.pop(0)
+            group['out'] = items.pop(0)
+
+        out.append(group)
+
+    return out
