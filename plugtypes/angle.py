22b2526e27a3bb58492b897dd0df51382cbbd473
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2022-07-28 03:48:37 +0300

    Update angle.py

--- plugtypes/angle.py
+++ plugtypes/angle.py
@@ -47,20 +47,3 @@ class Angle:
                 args = [value]
 
         r.plugs.Attribute.set(self, *args, **kwargs)
-
-    # def asPositive(self):
-    #     """
-    #     If this angle is a negative number, returns
-    #
-    #     :return:
-    #     """
-
-    def unwind(self):
-        """
-        Removes 360 winds from this angle, for example an angle of 400 becomes 40.
-
-        :return: This angle, unwound.
-        :rtype: :class:`~paya.runtime.plugs.Angle`
-        """
-        thisAbs = self.abs()
-        
