7cefad3a1b67f646969db5395851eb1670219857
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2022-07-31 20:20:49 +0300

    Update nurbsutil.py
    Removed conformUParamArg() to simplify overall implementation

--- lib/nurbsutil.py
+++ lib/nurbsutil.py
@@ -14,31 +14,6 @@ def getKnotList(numCVs, degree):
     tail = [numbers[-1]] * degree
     return head + numbers[1:-1] + tail
 
-def conformUParamArg(uParam):
-    """
-    If *uParam* is a component, returns its float form; otherwise returns
-    an attribute instance.
-
-    :param uParam: the U curve parameter argument to conform
-    :type uParam: float, str,
-        :class:`~paya.runtime.comps.NurbsCurveParameter`,
-        :class:`~paya.runtime.plugs.Math1D`
-    :return: The conformed parameter.
-    """
-    if isinstance(uParam, (float, r.Attribute)):
-        return uParam
-
-    if isinstance(uParam, r.Component):
-        return float(uParam)
-
-    if isinstance(uParam, str):
-        if '.u[' in uParam:
-            return r.Component(uParam)
-
-        return r.Attribute(uParam)
-
-    raise TypeError("Can't conform U parameter: {}".format(uParam))
-
 def itemsAsBezierAnchors(items):
     """
     :param items: indices, point positions, or whatever else can be mapped
