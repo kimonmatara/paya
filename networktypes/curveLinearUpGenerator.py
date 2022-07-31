b1e109d19ece3b0ba4abd2c37132ebe424e4d570
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2022-07-31 09:15:55 +0300

    Update curveLinearUpGenerator.py
    Doc cleanup

--- networktypes/curveLinearUpGenerator.py
+++ networktypes/curveLinearUpGenerator.py
@@ -3,12 +3,33 @@ import paya.runtime as r
 from paya.util import short
 
 
-
 class CurveLinearUpGenerator(r.networks.CurveUpGenerator):
+    """
+    Curve up vector generator with linearly interpolated vector keys. Behaves
+    similarly to an IK spline handle.
+    """
 
     @classmethod
     @short(interpolation='i')
-    def create(cls, curve, paramUpVectorKeys, interpolation=None):
+    def create(cls, curve, paramUpVectorKeys, interpolation='Linear'):
+        """
+        :param curve: the main curve
+        :type curve: str, :class:`~paya.runtime.nodes.NurbsCurve`,
+            :class:`~paya.runtime.plugs.NurbsCurve`,
+            :class:`~paya.runtime.nodes.Transform`
+        :param paramUpVectorKeys: zipped param: upVector pairs, defining
+            'known' up vectors at specific parameters between which to
+            interpolate
+        :param interpolation/i: this can be either one enum key or index
+            for the value interpolation, or a list of them
+            (one per key: value pair) The enums are:
+
+            -   0: 'None'
+            -   1: 'Linear' (the default)
+            -   2: 'Smooth'
+            -   3: 'Spline'
+        :return:
+        """
         curve = _po.asGeoPlug(curve, ws=True)
 
         with r.Name(curve.node().basename(sts=True), 'linear_up_gen'):
