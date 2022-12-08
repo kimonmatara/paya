8f4eb55c7aeadd95c65e23ab984d52111550cc8c
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2022-12-08 09:16:44 +0200

    Update mesh.py
    + initUtilUVSet()
    + initUVPin()

--- nodetypes/mesh.py
+++ nodetypes/mesh.py
@@ -1,3 +1,7 @@
+import paya.runtime as r
+from paya.util import short
+
+
 class Mesh:
 
     #-----------------------------------------------------|    Abstract I/O
@@ -13,3 +17,63 @@ class Mesh:
     @property
     def localGeoOutput(self):
         return self.attr('outMesh')
+
+    #-----------------------------------------------------|    Attachments
+
+    @short(name='n')
+    def initUtilUVSet(self, name='rig_util'):
+        """
+        Creates an auto projected UV set for attachments etc.
+        """
+        if self.hasHistory():
+            raise RuntimeError(
+                "Can't create utility UV set on a mesh with history.")
+
+        if name in self.getUVSetNames():
+            r.warning("Utility UV set 'rig_util' already exists.")
+            return self
+
+        numFaces = self.numFaces()
+
+        r.polyAutoProjection(
+            '{}.f[:{}]'.format(self, numFaces-1),
+            lm=0,
+            pb=0,
+            ibd=1,
+            cm=1,
+            l=2,
+            sc=1,
+            o=0,
+            p=12,
+            uvSetName=name,
+            ps=0.2,
+            ws=0,
+            ch=False
+        )
+
+        r.select(cl=True)
+        return self
+
+    @short(uvSet='uvs',
+           vertexIndices='vi',
+           uvPairs='uvp')
+    def initUVPin(self,
+                  uvPairs=None,
+                  vertexIndices=None,
+                  uvSet=None):
+        if uvSet is None:
+            uvSet = self.getCurrentUVSetName()
+
+        if vertexIndices is not None:
+            uvPairs = []
+
+            for vertexIndex in vertexIndices:
+                point = r.pointPosition(self.comp('vtx')[vertexIndex], world=True)
+                uv = self.getUVAtPoint(point, space='world', uvSet=uvSet)
+                uvPairs.append(uv)
+
+        return r.nodes.UvPin.create(
+            geometry=self,
+            uvPairs=uvPairs,
+            uvSet=uvSet
+        )
