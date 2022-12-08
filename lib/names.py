cfb0ecfd462a461400869daae00085e1bd67461a
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2022-12-08 09:33:18 +0200

    Update names.py
    * Rationalised makeName(), make()
    * Cleaned up dosctrings

--- lib/names.py
+++ lib/names.py
@@ -5,11 +5,16 @@ Node-naming utilities, usually called indirectly.
 import re
 
 import pymel.util as _pu
-
 import maya.cmds as m
+
 from paya.util import short, pad, undefined
-import paya.config as config
+
+from paya.config import config, takeUndefinedFromConfig
 import paya.lib.suffixes as _suf
+
+#----------------------------------------------------------|
+#----------------------------------------------------------|    FUNCTIONAL
+#----------------------------------------------------------|
 
 def legalise(name):
     """
@@ -17,6 +22,7 @@ def legalise(name):
 
     :param str name: the string to conform
     :return: The Maya name.
+    :rtype: :class:`str`
     """
     if name:
         name = name.strip()
@@ -37,6 +43,7 @@ def legalise(name):
         return '_'
 
 @short(padding='pad')
+@takeUndefinedFromConfig
 def conformElems(*elems, padding=undefined):
     """
     Cleans up user name elements wrangled by :class:`Name`.
@@ -49,9 +56,6 @@ def conformElems(*elems, padding=undefined):
     :rtype: list
     """
     out = []
-
-    if padding is undefined:
-        padding = config['padding']
 
     for elem in _pu.expandArgs(*elems):
         if isinstance(elem, int):
@@ -101,62 +105,120 @@ def shorten(
         shortName = dagElems[-1]
         underElems = shortName.split('_')
 
-        if len(underElems) > 1:
-            lastElem = underElems[-1]
+        lastElem = underElems[-1]
 
-            if _suf.isTypeSuffix(lastElem):
-                del(underElems[-1])
-                shortName = '_'.join(underElems)
+        if _suf.isTypeSuffix(lastElem):
+            del(underElems[-1])
+            shortName = '_'.join(underElems)
 
-            dagElems[-1] = shortName
-            name = ''.join(dagElems)
+        dagElems[-1] = shortName
+        name = ''.join(dagElems)
 
     return name
 
+#----------------------------------------------------------|
+#----------------------------------------------------------|    CONTEXTUAL
+#----------------------------------------------------------|
 
 class Name:
+    """
+        Context manager. Accumulates prefixes for default node names, and
+        provides other useful overrides.
+
+        :param \*elems: one or more name elements to combine into a prefix
+        :type \*elems: :class:`str`, :class:`int`
+        :param suffix/suf: if this is a string, it will be used a suffix for all
+            nodes; if it's ``True``, appropriate suffixes will be looked up;
+            if it's ``False``, no suffixes will be applied; defaults to
+            enclosing overrides, or the namesake configuration flag
+        :type suffix/suf: :class:`str`, :class:`bool`
+        :param padding/pad: the padding depth for any integer elements;
+            defaults to enclosing overrides, or the namesake configuration
+            flag
+        :type padding/pad: :class:`int`
+        :param bool inherit/i: inherit elements from enclosing :class:`Name`
+            blocks; defaults to ``True``
+        :param namespace/ns: set the namespace for the block; if omitted,
+            no namespace will be set
+        :type namespace/ns: :class:`str`
+        :param bool relativeNames/rel: switches to relative name lookups for
+            the block; defaults to enclosing overrides
+        :param bool allowUnprefixedSuffixes/aus: where no name elements have
+            been accumulated, allows, for example, a transform to be named
+            ``XFRM`` rather than ``transform1``; defaults to enclosing
+            overrides or the namesake configuration flag
+    """
     __elems__ = []
-    __suffix__ = config['suffixNodes']
-    __padding__ = undefined
-    __inherit__ = True
 
     @short(suffix='suf',
            padding='pad',
-           inherit='i')
+           inherit='i',
+           namespace='ns',
+           relativeNames='rel',
+           allowUnprefixedSuffixes='aus')
     def __init__(self,
                  *elems,
                  suffix=undefined,
                  padding=undefined,
-                 inherit=undefined):
+                 inherit=True,
+                 namespace=undefined,
+                 relativeNames=undefined,
+                 allowUnprefixedSuffixes=undefined):
+
         self.elems = list(_pu.expandArgs(*elems))
         self.suffix = suffix
         self.padding = padding
         self.inherit = inherit
+        self.namespace = namespace
+        self.relativeNames = relativeNames
+        self.allowUnprefixedSuffixes = allowUnprefixedSuffixes
 
     def __enter__(self):
-        self._prev_elems = Name.__elems__
-        self._prev_suffix = Name.__suffix__
-        self._prev_padding = Name.__padding__
-        self._prev_inherit = Name.__inherit__
+        # Elements
+        self._prev_elems = Name.__elems__[:]
 
-        Name.__elems__ = Name.__elems__ + self.elems
+        if self.inherit:
+            Name.__elems__ = self._prev_elems + self.elems
+        else:
+            Name.__elems__ = self.elems
 
-        if self.suffix is not undefined:
-            Name.__suffix__ = self.suffix
+        # Config overrides
+        overrides = {k:v for k, v in zip(
+            ['suffix', 'padding', 'allowUnprefixedSuffixes'],
+            [self.suffix, self.padding, self.allowUnprefixedSuffixes],
+            ) if v is not undefined}
 
-        if self.padding is not undefined:
-            Name.__padding__ = self.padding
+        self._prev_config = config.copy()
+        config.update(overrides)
 
-        if self.inherit is not undefined:
-            Name.__inherit__ = self.inherit
+        # Namespace
+        if self.namespace is not undefined:
+            self._prevNamespace = m.namespaceInfo(currentNamespace=True)
+
+            if not m.namespace(exists=self.namespace):
+                m.namespace(addNamespace=self.namespace)
+
+            m.namespace(setNamespace=self.namespace)
+
+        if self.relativeNames is not undefined:
+            self._prevRelativeNames = m.namespace(q=True, rel=True)
+            m.namespace(relativeNames=self.relativeNames)
 
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        # Elements
         Name.__elems__ = self._prev_elems
-        Name.__suffix__ = self._prev_suffix
-        Name.__padding__ = self._prev_padding
-        Name.__inherit__ = self._prev_inherit
+
+        # Config overrides
+        config.clear()
+        config.update(self._prev_config)
+
+        if self.namespace is not undefined:
+            m.namespace(setNamespace=self._prevNamespace)
+
+        if self.relativeNames is not undefined:
+            m.namespace(relativeNames=self._prevRelativeNames)
 
         return False
 
@@ -164,26 +226,52 @@ class Name:
     @short(nodeType='nt',
            transform='xf',
            suffix='suf',
+           allowUnprefixedSuffixes='aus',
            padding='pad',
            inherit='i',
            control='ct')
+    @takeUndefinedFromConfig
     def make(cls,
              *elems,
              nodeType=None,
              transform=None,
              suffix=undefined,
+             allowUnprefixedSuffixes=undefined,
              padding=undefined,
-             inherit=undefined,
+             inherit=True,
              control=False):
+        """
+        Constructs Maya node names.
 
-        if suffix is undefined:
-            suffix = Name.__suffix__
-
-        if padding is undefined:
-            padding = Name.__padding__
-
-        if inherit is undefined:
-            inherit = Name.__inherit__
+        :param \*elems: one or more name elements to combine into a prefix
+        :type \*elems: :class:`str`, :class:`int`
+        :param nodeType/nt: a hint for the node type suffix; defaults to
+            ``None``
+        :type nodeType/nt: :class:`str`, ``None``
+        :param bool transform/xf: indicate that the name is intended for a
+            transform; defaults to ``None``
+        :param suffix/suf: if this is a string, it will be used a suffix for all
+            nodes; if it's ``True``, appropriate suffixes will be looked up;
+            if it's ``False``, no suffixes will be applied; defaults to
+            enclosing overrides, or the namesake configuration flag
+        :type suffix/suf: :class:`str`, :class:`bool`
+        :param bool allowUnprefixedSuffixes/aus: where no name elements have
+            been accumulated, allows, for example, a transform to be named
+            ``XFRM`` rather than ``transform1``; defaults to enclosing
+            overrides or the namesake configuration flag
+        :param padding/pad: the padding depth for any integer elements;
+            defaults to enclosing overrides, or the namesake configuration
+            flag
+        :type padding/pad: :class:`int`
+        :param bool inherit/i: inherit name elements from :class:`Name`
+            blocks; defaults to ``True``
+        :param bool control/ct: indicate that the name is for a control;
+            defaults to ``False``
+        :return: The constructed node name.
+        :rtype: :class:`str`
+        """
+        if control:
+            suffix = _suf.suffixes['payaControl']
 
         elems = list(_pu.expandArgs(*elems))
 
@@ -202,36 +290,28 @@ class Name:
             else:
                 isShape = False
 
-        # Don't append a suffix when there are no name elements
+        if suffix and not isinstance(suffix, str):
+            if nodeType:
+                suffix = _suf.suffixes.get(nodeType)
+            else:
+                suffix = None
 
         if elems:
-            if isinstance(suffix, str):
+            if suffix:
                 elems.append(suffix)
-
-            elif suffix:
-                if control:
-                    suffix = _suf.suffixes['payaControl']
-
-                else:
-                    if nodeType:
-                        elems.append(_suf.suffixes.get(nodeType, nodeType))
 
             name = '_'.join(elems)
 
-            if isShape:
-                name += 'Shape'
+        else:
+            if suffix and allowUnprefixedSuffixes:
+                name = suffix
+            elif nodeType:
+                name = nodeType+'1'
+            else:
+                name = ''
 
-            out = legalise(name)
-            return out
+        if isShape:
+            name += 'Shape'
 
-        # No elements; if we have a node type, use it to improvise a name
-
-        if nodeType:
-            out = nodeType+'1'
-
-            if isShape:
-                out += 'Shape'
-
-            return out
-
-        return legalise('')
+        name = legalise(name)
+        return name
