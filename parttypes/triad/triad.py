a11341f95a30fabe13d3b2dae2df06ff9d77a0cf
kimonmatara <87778438+kimonmatara@users.noreply.github.com>
2022-12-15 22:44:24 +0200

    Update triad.py
    WIP--ongoing

--- parttypes/triad/triad.py
+++ parttypes/triad/triad.py
@@ -1,5 +1,7 @@
+from importlib import reload
 import paya.runtime as r
 import paya.lib.mathops as _mo
+reload(_mo)
 from paya.util import short
 from paya.config import takeUndefinedFromConfig, undefined
 
@@ -12,143 +14,252 @@ class Triad(r.parts.Part):
 
     @r.partCreator
     @takeUndefinedFromConfig
-    def create(self,
-               points,
-               upVector,
-               numTopTwists=3,
-               numBtmTwists=3,
-               bevelLength=None,
-               endIkCtMatrix=None,
-               endRotCtMatrix=None,
-               downAxis=undefined,
-               upAxis=undefined,
-               polePoint=None,
-               poleLength=None
-               ):
-        """
-        :param points:
-        :param topTwist:
-        :param bottomTwist:
-        :return:
-        """
-        """
-        Resolve points list against 'bevelLength'
-        
-        TMP
-            Draw temp chain
-        
-        IK
-            Derive chain
-            Draw cts
-            Hook up, with ss
-        FK
-            Derive chain
-            Draw cts
-            Drive chain, with ss
-        
-        BL
-            Derive blend chain
-            IK / FK blend
-            
-        TW
-            Derive twist chain
-        """
-        #---------------------------------------------------|
-        #---------------------------------------------------|    Init
-        #---------------------------------------------------|
+    def create(
+            self,
+            points,
+            upVector,
+            endIkCtMatrix=None,
+            endRotCtMatrix=None,
+            downAxis=undefined,
+            upAxis=undefined,
+            poleCtMatrix=None,
+            createIkLine=True,
+            polePoint=None, # ignored if poleCtMatrix
+            poleLength=1.0 # ignored if polePoint or poleCtMatrix
+    ):
+        #--------------------------------------|    Init
 
-        points = self._resolvePoints(points, bevelLength=bevelLength)
+        if len(points) is not 3:
+            raise ValueError("Need three points.")
+
+        points = [r.data.Point(point) for point in points]
         upVector = r.data.Vector(upVector)
 
-        #-----------------------------------------------------------|
-        #-----------------------------------------------------------|    TMP
-        #-----------------------------------------------------------|
+        #--------------------------------------|    Tmp
 
-        with r.Name('tmp'):
+        with r.Name('tmp', padding=2):
             tmpChain = r.Chain.createFromPoints(
                 points,
                 upVector,
                 downAxis=downAxis,
                 upAxis=upAxis,
-                parent=self.tree['joints'].node(inheritsTransform=False)
+                parent=self.tree['joints'].node()
             )
 
-        #-----------------------------------------------------------|
-        #-----------------------------------------------------------|    IK
-        #-----------------------------------------------------------|
+        #--------------------------------------|    IK
 
         with r.Name('ik'):
-            ikSystem = self._createIkSystem(
-                tmpChain,
-                downAxis,
-                upAxis,
-                endIkCtMatrix=endIkCtMatrix,
-                upVector=upVector,
-                polePoint=polePoint,
-                poleLength=poleLength
-            )
+            ikResult = self._createIkSystem(tmpChain,
+                                            downAxis,
+                                            upAxis,
+                                            upVector,
+                                            endIkCtMatrix=endIkCtMatrix,
+                                            poleCtMatrix=poleCtMatrix,
+                                            polePoint=polePoint,
+                                            poleLength=poleLength,
+                                            createIkLine=createIkLine)
 
-        #-----------------------------------------------------------|
-        #-----------------------------------------------------------|    FK
-        #-----------------------------------------------------------|
+        #--------------------------------------|    FK
 
         with r.Name('fk'):
-            fkSystem = self._createFkSystem(
-                tmpChain,
-                downAxis,
-                upAxis,
-                endRotCtMatrix=endRotCtMatrix,
-                upVector=upVector
-            )
+            fkResult = self._createFkSystem(tmpChain,
+                                            downAxis,
+                                            upAxis,
+                                            upVector)
 
-        #-----------------------------------------------------------|
-        #-----------------------------------------------------------|    BL
-        #-----------------------------------------------------------|
+        #--------------------------------------|    Blend
 
-        hasTwists = numTopTwists or numBtmTwists
-
-        with r.Name('blend' if hasTwists else 'bind'):
-            blSystem = self._createBlendSystem(
-                fkSystem,
-                ikSystem
-            )
-
-        #-----------------------------------------------------------|
-        #-----------------------------------------------------------|    TW
-        #-----------------------------------------------------------|
-
-        if hasTwists:
-            with r.Name('bind'):
-                twSystem = self._createTwistSystem(blSystem)
-
-        #-----------------------------------------------------------|
-        #-----------------------------------------------------------|    Finish
-        #-----------------------------------------------------------|
-
-        """
-        Tag bind joints
-        """
-        raise NotImplementedError('Finish')
+        with r.Name('blend'):
+            self._createBlendSystem(tmpChain, fkResult, ikResult)
 
     #-------------------------------------------------------|
-    #-------------------------------------------------------|    PARTIAL
+    #-------------------------------------------------------|    PARTIALS
     #-------------------------------------------------------|
 
-    def _resolvePoints(self, points, bevelLength=None):
-        num = len(points)
+    def _createIkSystem(self,
+                        tmpChain,
+                        downAxis,
+                        upAxis,
+                        upVector,
+                        poleCtMatrix=None,
+                        polePoint=None,
+                        endIkCtMatrix=None,
+                        poleLength=1.0,
+                        createIkLine=True):
 
-        if num is 3:
-            if bevelLength is not None:
-                points = _mo.bevelTriadPoints(points, bevelLength)
+        #--------------------------------------|    Derive chain
 
-        elif num is 4:
-            if bevelLength is not None:
-                raise ValueError("bevelLengthling needs three points.")
+        with r.Name(padding=2):
+            chain = tmpChain[:3].duplicate()
 
-            points = [r.data.Point(x) for x in points]
+        #--------------------------------------|    Draw pole controls
 
+        if poleCtMatrix is not None:
+            poleCtMatrix = r.data.Matrix(poleCtMatrix)
         else:
-            raise ValueError("Provide three or four points.")
+            if polePoint is not None:
+                polePoint = r.data.Point(polePoint)
+            else:
+                inline = chain.isInline()
 
-        return points
+                if inline:
+                    chordVec = tmpChain[2].gwp()-tmpChain[0].gwp()
+                    kneeVec = chordVec.cross(upVector)
+                else:
+                    v1, v2 = tmpChain.vectors()
+                    kneeVec = v1.normal()-v2.normal()
 
+                polePoint = tmpChain[1].gwp() + (kneeVec.normal() * poleLength)
+
+            poleCtMatrix = r.data.Matrix()
+            poleCtMatrix.t = polePoint
+
+        # Draw
+        with r.Name('pole'):
+            poleCts = r.createControls(
+                worldMatrix=poleCtMatrix,
+                parent=self.tree['controls']['ik'].node(),
+                keyable=['t'],
+                shape='sphere'
+            )
+
+        #--------------------------------------|    Draw end controls
+
+        if endIkCtMatrix:
+            drawMatrix = r.data.Matrix(endIkCtMatrix)
+        else:
+            drawMatrix = r.data.Matrix()
+            drawMatrix.t = chain[2].getWorldPosition()
+
+        with r.Name('end'):
+            endCts = r.createControls(
+                numControls=2,
+                worldMatrix=drawMatrix,
+                keyable=['t', 'r', 'ro'],
+                parent=self.tree['controls']['ik'].node(),
+                pickWalkParent=poleCts[0],
+                pickWalkInsets=False
+            )
+
+        #--------------------------------------|    Create IK handle
+
+        with r.Name('end'):
+            endIkh = chain.createIkHandle(jitter=True,
+                                          upVector=upVector)
+
+            endIkh.setParent(self.tree['util'].node(it=False))
+            endIkh.hide()
+            endCts[1].attr('wm').drive(endIkh, ws=True, mo=True)
+
+        #--------------------------------------|    Assign pole point
+
+        livePolePoint = poleCts[1].getWorldPosition(plug=True)
+
+        endIkh.setPolePoint(livePolePoint,
+                            maintainOffset=True)
+
+        #--------------------------------------|    Add auto-follow for pole
+
+        chordStart = chain[0].getWorldPosition() \
+            ^ self.node().getWorldMatrix(plug=True, asOffset=True)
+
+        chordEnd = endCts[1].getWorldPosition(plug=True)
+        chordVec = chordEnd-chordStart
+        chordLen = chordVec.length()
+        nativeChainLen = chain.length() * self.getPartScale(plug=True)
+        chordLen = chordLen.maxClamp(nativeChainLen)
+        targetLen = chordLen * 0.5
+
+        mtx = r.createMatrix(
+            downAxis,
+            chordVec,
+            upAxis,
+            upVector*self.node().getWorldMatrix(plug=True, asOffset=True),
+            t=chordStart+(chordVec.normal() * targetLen)
+        ).pk(t=True, r=True)
+
+        mtx = self.getPartScaleMatrix(plug=True) * mtx
+
+        poleCts[0].createAnimSpace(['auto'], [mtx],
+                                   translate=True, rotate=True)
+
+        #-------------------------------------------|    Create feedback line
+
+        chordEnd = chordStart + (chordVec.normal() * chordLen)
+
+        if createIkLine:
+            with r.Name('feedback'):
+                curve = r.nodes.NurbsCurve.create(
+                    [chordStart, livePolePoint, chordEnd],
+                    displayType='Template',
+                    degree=1
+                ).toTransform()
+
+                curve.setParent(self.tree['util'].node(it=False))
+
+        out = {'chain': chain}
+
+        if createIkLine:
+            out['curve'] = curve
+
+        return out
+
+    def _createFkSystem(self, tmpChain, downAxis, upAxis, upVector):
+
+        #-------------------------------------------|    Derive chain
+
+        with r.Name(padding=2):
+            chain = tmpChain.duplicate()
+
+        #-------------------------------------------|    Draw controls
+
+        controls = []
+        rotateOrder = _mo.inventRotateOrder(downAxis, upAxis)
+
+        for i, joint in enumerate(chain[:2]):
+            keyable = ['ro']
+
+            if i is 1:
+                keyable.append('r'+_mo.absAxis(upAxis))
+            else:
+                keyable.append('r')
+
+            control = r.createControl(
+                worldMatrix=joint.getWorldMatrix(),
+                rotateOrder=rotateOrder,
+                keyable=keyable,
+                parent=controls[-1] if controls \
+                    else self.tree['controls']['fk'].node(),
+                pickWalkParent=controls[-1] if controls else None,
+                shape='diamond'
+            )
+
+            control.attr('wm').decomposeAndApply(joint, ws=True, r=True)
+            controls.append(control)
+
+        return {'chain': chain}
+
+    def _createBlendSystem(self, tmpChain, fkResult, ikResult):
+        node = self.node()
+        ikOn = node.addAttr('ikOn', k=True, min=0, max=1, dv=1)
+        ikOff = node.addAttr('ikOff', min=0, max=1, dv=0)
+        (1-ikOn) >> ikOff
+        ikOff.lock()
+
+        ikOn >> self.tree['controls']['ik'].node().attr('v')
+        ikOff >> self.tree['controls']['fk'].node().attr('v')
+
+        try:
+            ikOn >> ikResult['curve'].attr('v')
+        except KeyError:
+            pass
+
+        fkChain = fkResult['chain']
+        ikChain = ikResult['chain']
+
+        with r.Name(padding=2):
+            blChain = tmpChain.duplicate()
+
+        for fkJt, ikJt, blJt in zip(fkChain, ikChain, blChain):
+            mtx = fkJt.wm.blend(ikJt.wm, weight=ikOn)
+            mtx.drive(blJt, ws=True, uop=False)
